0ed043c405ea7aa37ef8abcd260a3a69e3c3539d
Bashar Alwarad <beelwarad52@gmail.com>
2025-02-28 10:10:14 +0100

    features offers

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
@@ -18,7 +17,7 @@ export const ListingItem = ({ listing }) => {
 
   return (
     <div className="card w-full bg-base-100 shadow-xl p-4">
-      <div className="w-1/2 mx-auto">
+      <div className="w-1/4 mx-auto">
         <Carousel showThumbs={false} infiniteLoop autoPlay>
           {imageUrls?.map((url, index) => (
             <div key={index}>
@@ -30,39 +29,45 @@ export const ListingItem = ({ listing }) => {
             </div>
           ))}
         </Carousel>
+        <p className="text-gray-600">📍 {location}</p>
       </div>
 
       <div className="p-4 space-y-2">
         <h3 className="text-xl font-bold">{name}</h3>
-        <p className="text-gray-600">📍 {location}</p>
-        <p className="text-gray-800">
-          {offer ? (
-            <span className="text-red-500 font-semibold">
-              Discounted Price: ${discountedPrice}
-            </span>
-          ) : (
-            <span>Regular Price: ${regularPrice}</span>
-          )}
-        </p>
         <table className="table-auto w-full text-gray-700">
           <thead>
             <tr>
+              {offer ? (
+                <th className="px-4 py-2">Discounted Price</th>
+              ) : (
+                <th className="px-4 py-2">Regular Price</th>
+              )}
               <th className="px-4 py-2">🛏 Bedrooms</th>
               <th className="px-4 py-2">🛁 Bathrooms</th>
-              <th className="px-4 py-2">📐 Area</th>
               <th className="px-4 py-2">🏡 Type</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td className="px-4 py-2 text-center">{bedrooms || 'X'}</td>
+              {offer ? (
+                <td className="px-4 py-2 text-center">
+                  <span className="text-red-500 font-semibold">
+                    $ {discountedPrice || 'X'}
+                  </span>
+                </td>
+              ) : (
+                <td className="px-4 py-2 text-center">
+                  $ {regularPrice || 'X'}
+                </td>
+              )}
+              <td className="px-4 py-2 text-center"> {bedrooms || 'X'}</td>
               <td className="px-4 py-2 text-center">{bathrooms || 'X'}</td>
-              <td className="px-4 py-2 text-center">{listing.area || 'X'}</td>
-              <td className="px-4 py-2 text-center">for {type || 'X'}</td>
+              <td className="px-4 py-2 text-center">
+                <p className="badge badge-secondary uppercase">{type}</p>
+              </td>
             </tr>
           </tbody>
         </table>
-        <p className="badge badge-secondary uppercase">{type}</p>
       </div>
     </div>
   );
---
+++ client/src/pages/Offers.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import { ListingItem, Spinner } from '../components';
+
+export const Offers = () => {
+  const [listings, setListings] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchedListing] = useState(null);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const listingsRef = collection(db, 'listings');
+        const q = query(
+          listingsRef,
+          where('offer', '==', true),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        );
+        const querySnap = await getDocs(q);
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchedListing(lastVisible);
+
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error('Could not fetch listings');
+      }
+    };
+
+    fetchListings();
+  }, []);
+
+  const onFetchMoreListings = async () => {
+    try {
+      const listingsRef = collection(db, 'listings');
+      const q = query(
+        listingsRef,
+        where('offer', '==', true),
+        orderBy('timestamp', 'desc'),
+        startAfter(lastFetchedListing),
+        limit(10)
+      );
+      const querySnap = await getDocs(q);
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchedListing(lastVisible);
+
+      const listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings((prevState) => [...prevState, ...listings]);
+      setLoading(false);
+    } catch (error) {
+      toast.error('Could not fetch listings');
+    }
+  };
+
+  return (
+    <div className="category">
+      <header>
+        <p className="pageHeader">Offers</p>
+      </header>
+
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+          <br />
+          <br />
+          {lastFetchedListing && (
+            <p className="loadMore" onClick={onFetchMoreListings}>
+              Load More
+            </p>
+          )}
+        </>
+      ) : (
+        <p>No offers available</p>
+      )}
+    </div>
+  );
+};
