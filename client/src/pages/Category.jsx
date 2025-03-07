ef6d3b6e7f4c51466172f6d4e74d41b7a8b33954
Bashar Alwarad <beelwarad52@gmail.com>
2025-03-07 07:55:52 +0100

    feature: loade more listing to category

--- client/src/pages/Category.jsx
+++ client/src/pages/Category.jsx
@@ -7,6 +7,7 @@ import {
   where,
   orderBy,
   limit,
+  startAfter,
 } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { toast } from 'react-toastify';
@@ -15,6 +16,8 @@ import { ListingItem, Spinner } from '../components';
 export const Category = () => {
   const [listings, setListings] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [lastFetchedListings, setLastFetchedListings] = useState(null);
+
   const { categoryName } = useParams();
   useEffect(() => {
     const fetchListings = async () => {
@@ -23,12 +26,13 @@ export const Category = () => {
         const q = query(
           listingsRef,
           where('type', '==', categoryName),
-          //   where('category', '==', categoryName),
           orderBy('timestamp', 'desc'),
-          limit(10)
+          limit(1)
         );
 
         const querySnapshot = await getDocs(q);
+        const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
+        setLastFetchedListings(lastVisible);
         const listings = querySnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
@@ -44,6 +48,38 @@ export const Category = () => {
     fetchListings();
   }, [categoryName]);
 
+  const onFetchMoreListings = async () => {
+    try {
+      const listingsRef = collection(db, 'listings');
+      const q = query(
+        listingsRef,
+        where('type', '==', categoryName),
+        orderBy('timestamp', 'desc'),
+        startAfter(lastFetchedListings),
+        limit(1)
+      );
+
+      const querySnapshot = await getDocs(q);
+      const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
+      if (lastVisible === undefined) {
+        toast.info('No more listings to fetch');
+        setLoading(false);
+        return;
+      }
+      setLastFetchedListings(lastVisible);
+      const listings = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log(lastVisible);
+      setListings((pre) => [...pre, ...listings]);
+      setLoading(false);
+    } catch (error) {
+      toast.error('Error: Fetching listings failed');
+      setLoading(false);
+    }
+  };
+
   if (loading) return <Spinner />;
   if (listings.length <= 0) return <p>No listings found</p>;
 
@@ -53,6 +89,13 @@ export const Category = () => {
       {listings?.map((listing) => (
         <ListingItem key={listing.id} listing={listing} />
       ))}
+      <button
+        className="btn btn-primary"
+        onClick={onFetchMoreListings}
+        disabled={loading}
+      >
+        {loading ? 'Loading...' : 'Load more'}
+      </button>
     </div>
   );
 };
